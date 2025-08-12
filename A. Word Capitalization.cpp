018b1801ab4153e213bbc17b4f36e981b789e1b9
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-12 20:26:04 +0530

    A. Word Capitalization.cpp

---
+++ A. Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+    
+    if (!s.empty()) {
+        s[0] = toupper(s[0]);
+    }
+    
+    cout << s << endl;
+    return 0;
+}
